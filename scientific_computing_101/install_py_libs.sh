f79235863c5e8ff401abd2645691da59104a17c6
Poruri Sai Rahul <rporuri@enthought.com>
2017-05-20 02:54:49 +0530

    UPD : update all notebooks
    and add a new shell script that will create a virtualenv
    and install the relevant packages.
    
    	new file:   install_py_libs.sh
    	modified:   jupyter_notebook.ipynb
    	modified:   matplotlib.ipynb
    	modified:   numpy.ipynb
    	modified:   pandas.ipynb

---
+++ scientific_computing_101/install_py_libs.sh
@@ -0,0 +1,8 @@
+pip install virtualenv
+virtualenv scientific_computing_101
+cd scientific_computing_101
+source bin/activate
+which pip
+which python
+pip install --upgrade pip
+pip install jupyter notebook matplotlib
