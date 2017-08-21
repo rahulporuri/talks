daa20f4ef2d1fa8c2ce4ff7d98bca60b55b8844f
Poruri Sai Rahul <rporuri@enthought.com>
2017-08-20 19:45:03 -0500

    FIX : fix contribution to distutils.commands

--- py_entry_points/distutils_ext/distutils_ext_pkg/main.py
+++ py_entry_points/distutils_ext/distutils_ext_pkg/main.py
@@ -1,5 +1,20 @@
 from __future__ import print_function
+from distutils.core import Command
 
 
-def main():
-    print("Contributing ...")
+class distutils_ext_pkg(Command):
+    user_options = [('default-arg=', 'd',
+                      'default argument')
+                    ]
+
+    def initialize_options(self):
+        """abstract method on baseclass that needs to be overwritten"""
+        self.default_arg = None
+
+    def finalize_options(self):
+        """abstract method on baseclass that needs to be overwritten"""
+        self.default_arg = "What?"
+
+    def run(self):
+        """abstract method on baseclass that needs to be overwritten"""
+        print("Beep Boop Beep {}".format(self.default_arg))
--- py_entry_points/distutils_ext/setup.py
+++ py_entry_points/distutils_ext/setup.py
@@ -5,7 +5,7 @@ setup(
     name="distutils_ext_pkg",
     entry_points={
         "distutils.commands":[
-            "distutils_ext_pkg = distutils_ext_pkg.main:main"
+            "distutils_ext_pkg = distutils_ext_pkg.main:distutils_ext_pkg"
         ]
     }
 )
